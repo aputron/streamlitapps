79498d3a4f821314773b2cd2138a2b0f125ac1d0
aputron <64962874+aputron@users.noreply.github.com>
2024-10-07 18:19:42 +0000

    Update

--- FACS.py
+++ FACS.py
@@ -54,6 +54,7 @@ def facs_graph(files, labels, colour, width, height, y_lim, x_lim, bins, x_axis)
 
     # plot the graph using matplotlib and seaborn
     _fig, _ax = plt.subplots(figsize=(width, height))
+    _ax.legend_ = None
     graph = sns.histplot(df, multiple="layer", log_scale=True, element="poly", palette=colour, bins=bins)
     graph.set_ylim(y_lim)
     graph.set_xlim(x_lim)
